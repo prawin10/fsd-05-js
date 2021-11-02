985afab119bb7707bf332e861d841a8db67b7faf
prawin10 <prawinaga10@gmail.com>
2021-11-02 13:48:03 +0530

    js practise

--- app/color-flipper/main.js
+++ app/color-flipper/main.js
@@ -1,21 +1,24 @@
 // 1. Trigger event when user click on the event.
 let myButton = document.getElementById('color-generator');
-console.log(document);
-console.log(document.getElementById);
-console.log(myButton);
+ console.log(document);
+ console.log(document.getElementById);
+ console.log(myButton);
 
 myButton.addEventListener('click',function()
 {
-    console.log('Fabevy');
+   // console.log('generateRandomColor');(just how many click function in generateRandomColor).
+   let randomColor = generateRandomColor(),
+     bodyTag = document.getElementById('body-tag');
+    bodyTag.style.backgroundColor = randomColor;
 })
-
-// 2. Write a function to generate random color.
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// 2. Write a function to generate random color.
 
 function generateRandomColor(){
     let red, green, blue, style, 
@@ -27,9 +30,9 @@ function generateRandomColor(){
      green = getRandomInt(0, 255);
      blue = getRandomInt(0, 255);
 
-    console.log(red, green, blue);
+
 
     style = head + red + seperator + green + seperator + blue + tail;
 
-    console.log(style);
+    return style;
 }
